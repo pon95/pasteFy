a8116f113901ef3dee0c248a61a62d5e6e50c68a
pon95 <125143559+pon95@users.noreply.github.com>
2023-10-21 19:23:04 +0500

    Made authors function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 import json
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 # Загрузка данных из JSON-файла
 def load_data():
@@ -21,25 +21,34 @@ def save_data(data):
 def create_paste():
     if request.method == 'POST':
         text = request.form['text']
-        pastas = load_data()
-        print(pastas)
-        pasta_id = len(pastas) + 1
-        pastas[pasta_id] = text
-
-        # Сохраняем пасты в файл JSON
-        save_data(pastas)
-
+        author = request.form['nickname']
+        if text.strip() != '':
+            pastas = load_data()
+            pasta_id = len(pastas) + 1
+            pastas[pasta_id] = {
+                'author': author.strip() if author.strip() != '' else '--- не указано ---',
+                'text': text
+            }
+            save_data(pastas)
+        else:
+            return f'Похоже, вы создали пустой паст.\nПустые пасты не сохранаются.\n<a href="/">Назад</a>'
         return f"Ваша паста создана! <a href='/paste/{pasta_id}'>Посмотреть паст</a>"
 
     return render_template('index.html')
 
-@app.route('/paste/<int:pasta_id>')
+@app.route('/paste/<pasta_id>')
 def view_paste(pasta_id):
-    pastas = load_data()
-    pasta = pastas.get(pasta_id)
-    if pasta is not None:
-        return render_template('view_paste.html', pasta=pasta)
-    return "Пасты не найдено."
+    try:
+        pastas = load_data()
+        author = pastas.get(pasta_id).get('author')
+        pasta = pastas.get(pasta_id).get('text')
+        if pasta is not None:
+            return render_template('view_paste.html', pasta=pasta, author=author.capitalize())
+    except:
+        return render_template('404.html')
+@app.errorhandler(404)
+def err(e):
+    return render_template('404.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
